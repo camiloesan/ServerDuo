5e25393d52c95d217b2aca38787363d867c791dd
camiloesan <camiloesan@gmail.com>
2024-01-07 13:55:50 -0600

    documentation update

--- CommunicationService/IPartyManager.cs
+++ CommunicationService/IPartyManager.cs
@@ -7,15 +7,36 @@ namespace CommunicationService
     [ServiceContract]
     public interface IPartyValidator
     {
+        /// <summary>
+        /// Checks if a party with the partyCode provided is active
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <returns>Success status</returns>
         [OperationContract]
         bool IsPartyExistent(int partyCode);
 
+        /// <summary>
+        /// Checks if there´s a space available to join (<4)
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <returns>Availability</returns>
         [OperationContract]
         bool IsSpaceAvailable(int partyCode);
 
+        /// <summary>
+        /// If an user with the provided username is inside the lobby with provided partyCode
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <param name="username"></param>
+        /// <returns>Status</returns>
         [OperationContract]
         bool IsUsernameInParty(int partyCode, string username);
 
+        /// <summary>
+        /// Counts every player on party hashmap
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <returns>Total players in party</returns>
         [OperationContract]
         List<string> GetPlayersInParty(int partyCode);
     }
@@ -23,24 +44,60 @@ namespace CommunicationService
     [ServiceContract(CallbackContract = typeof(IPartyManagerCallback))]
     public interface IPartyManager
     {
+        /// <summary>
+        /// Notifies host that party has been created
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <param name="hostUsername"></param>
         [OperationContract(IsOneWay = true)]
         void NotifyCreateParty(int partyCode, string hostUsername);
 
+        /// <summary>
+        /// Notifies new player joining to every player
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <param name="username"></param>
         [OperationContract(IsOneWay = true)]
         void NotifyJoinParty(int partyCode, string username);
 
+        /// <summary>
+        /// Notifies every player in party a message (to chat)
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <param name="message"></param>
         [OperationContract(IsOneWay = true)]
         void NotifySendMessage(int partyCode, string message);
 
+        /// <summary>
+        /// Notifies every player in party if someone leaves lobby
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <param name="username"></param>
         [OperationContract(IsOneWay = true)]
         void NotifyLeaveParty(int partyCode, string username);
 
+        /// <summary>
+        /// Notifies every player in party if host has left lobby
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <param name="hostName"></param>
+        /// <param name="reason"></param>
         [OperationContract(IsOneWay = true)]
         void NotifyCloseParty(int partyCode, string hostName, string reason);
 
+        /// <summary>
+        /// Notifies game start to every player in party
+        /// </summary>
+        /// <param name="partyCode"></param>
         [OperationContract(IsOneWay = true)]
         void NotifyStartGame(int partyCode);
 
+        /// <summary>
+        /// Notifies every player in party if host has kicked a player (including kicked player)
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <param name="username"></param>
+        /// <param name="reason"></param>
         [OperationContract(IsOneWay = true)]
         void NotifyKickPlayer(int partyCode, string username, string reason);
     }
@@ -48,21 +105,45 @@ namespace CommunicationService
     [ServiceContract]
     public interface IPartyManagerCallback
     {
+        /// <summary>
+        /// Receives updated player dictionary
+        /// </summary>
+        /// <param name="playersInLobby"></param>
         [OperationContract]
         void PartyCreated(ConcurrentDictionary<string, IPartyManagerCallback> playersInLobby);
 
+        /// <summary>
+        /// Receives updated player dictionary
+        /// </summary>
+        /// <param name="playersInLobby"></param>
         [OperationContract]
         void PlayerJoined(ConcurrentDictionary<string, IPartyManagerCallback> playersInLobby);
 
+        /// <summary>
+        /// Receives message sent by someone
+        /// </summary>
+        /// <param name="messageSent"></param>
         [OperationContract]
         void MessageReceived(string messageSent);
 
+        /// <summary>
+        /// Receives updated player dictionary
+        /// </summary>
+        /// <param name="playersInLobby"></param>
         [OperationContract]
         void PlayerLeft(ConcurrentDictionary<string, IPartyManagerCallback> playersInLobby);
 
+
+        /// <summary>
+        /// Receives notification if player himself has been kicked
+        /// </summary>
+        /// <param name="reason"></param>
         [OperationContract]
         void PlayerKicked(string reason);
 
+        /// <summary>
+        /// Receives game started notification
+        /// </summary>
         [OperationContract]
         void GameStarted();
     }
--- CommunicationService/IUsersManager.cs
+++ CommunicationService/IUsersManager.cs
@@ -5,97 +5,254 @@ using System.ServiceModel;
 
 namespace CommunicationService
 {
+    /// <summary>
+    /// Handles user actions
+    /// </summary>
     [ServiceContract]
     public interface IUsersManager
     {
+        /// <summary>
+        /// Updates profile picture id to a user
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="pictureId"></param>
+        /// <returns>Rows affected</returns>
         [OperationContract]
         int UpdateProfilePictureByUserId(int userId, int pictureId);
 
+        /// <summary>
+        /// Gets all information about a player (except password)
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns>UserDTO user details</returns>
         [OperationContract]
         UserDTO GetUserInfoByUsername(string username);
 
+        /// <summary>
+        /// Adds an user to database
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>Rows affected</returns>
         [OperationContract]
         int AddUserToDatabase(UserDTO user);
 
+        /// <summary>
+        /// Deletes user from database
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns>Success status</returns>
         [OperationContract]
         bool DeleteUserFromDatabaseByUsername(String username);
 
+        /// <summary>
+        /// Checks if login credentials are valid
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>UserDTO if valid, null if not</returns>
         [OperationContract]
         UserDTO IsLoginValid(string username, string password);
 
+        /// <summary>
+        /// Checks if provided username exists in database
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns>True if exists, false if not</returns>
         [OperationContract]
         bool IsUsernameTaken(string username);
 
+        /// <summary>
+        /// Checks if provided email exists in database
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>True if exists, false if not</returns>
         [OperationContract]
         bool IsEmailTaken(string email);
 
+        /// <summary>
+        /// Adds a friend request to database
+        /// </summary>
+        /// <param name="usernameSender"></param>
+        /// <param name="usernameReceiver"></param>
+        /// <returns>Rows affected</returns>
         [OperationContract]
         int SendFriendRequest(string usernameSender, string usernameReceiver);
 
+        /// <summary>
+        /// Checks if sender has already sent a request to receiver
+        /// </summary>
+        /// <param name="usernameSender"></param>
+        /// <param name="usernameReceiver"></param>
+        /// <returns>True if a request if found, false if not</returns>
         [OperationContract]
         bool IsFriendRequestAlreadyExistent(string usernameSender, string usernameReceiver);
 
+        /// <summary>
+        /// Adds request user id's to friendship table
+        /// </summary>
+        /// <param name="friendRequest"></param>
+        /// <returns>True if succeded, false if not</returns>
         [OperationContract]
         bool AcceptFriendRequest(FriendRequestDTO friendRequest);
 
+        /// <summary>
+        /// Deletes from friend request table the request
+        /// </summary>
+        /// <param name="friendRequestId"></param>
+        /// <returns>True if succeded, false if not</returns>
         [OperationContract]
         bool RejectFriendRequest(int friendRequestId);
 
+        /// <summary>
+        /// Gets user's friend requests
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>List FriendRequestDTO</returns>
         [OperationContract]
         List<FriendRequestDTO> GetFriendRequestsList(int userId);
 
+        /// <summary>
+        /// Gets user's friends
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>List FriendshipDTO</returns>
         [OperationContract]
         List<FriendshipDTO> GetFriendsList(int userId);
 
+        /// <summary>
+        /// Gets user´s online friends
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>List FriendshipDTO</returns>
         [OperationContract]
         List<FriendshipDTO> GetOnlineFriends(int userId);
 
+        /// <summary>
+        /// Deletes friendship from database
+        /// </summary>
+        /// <param name="friendshipId"></param>
+        /// <returns>True if success, false if not</returns>
         [OperationContract]
         bool DeleteFriendshipById(int friendshipId);
 
+        /// <summary>
+        /// Checks if receiver username is sender's friend
+        /// </summary>
+        /// <param name="senderUsername"></param>
+        /// <param name="receiverUsername"></param>
+        /// <returns>True if is friend, false if not</returns>
         [OperationContract]
         bool IsAlreadyFriend(string senderUsername, string receiverUsername);
 
+        /// <summary>
+        /// Checks if user is logged in (online)
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns>True if is online, false if not</returns>
         [OperationContract]
         bool IsUserAlreadyLoggedIn(string username);
 
+        /// <summary>
+        /// Sends a confirmation code to provided email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="lang"></param>
+        /// <returns>confirmation code</returns>
         [OperationContract]
         int SendConfirmationCode(string email, string lang);
 
+        /// <summary>
+        /// Modifies password by email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="newPassword"></param>
+        /// <returns>Rows affected</returns>
         [OperationContract]
         int ModifyPasswordByEmail(string email, string newPassword);
 
+        /// <summary>
+        /// Adds user to blocked user's list
+        /// </summary>
+        /// <param name="blockerUsername"></param>
+        /// <param name="blockedUsername"></param>
+        /// <returns>Rows affected: 1 if succeed, 2 if banned definitely</returns>
         [OperationContract]
         int BlockUserByUsername(string blockerUsername, string blockedUsername);
 
+        /// <summary>
+        /// Deletes user from user's block list
+        /// </summary>
+        /// <param name="blockId"></param>
+        /// <returns>Rows affected</returns>
         [OperationContract]
         int UnblockUserByBlockId(int blockId);
 
+        /// <summary>
+        /// Checks if user is blocked by another user
+        /// </summary>
+        /// <param name="usernameBlocker"></param>
+        /// <param name="usernameBlocked"></param>
+        /// <returns>True if is blocked, false if not</returns>
         [OperationContract]
         bool IsUserBlockedByUsername(string usernameBlocker, string usernameBlocked);
 
+        /// <summary>
+        /// Gets user's blocked list
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>List UserBlockedDTO</returns>
         [OperationContract]
         List<UserBlockedDTO> GetBlockedUsersListByUserId(int userId);
 
+        /// <summary>
+        /// Gets top 10 global winners
+        /// </summary>
+        /// <returns>List UserDTO</returns>
         [OperationContract]
         List<UserDTO> GetTopTenWinners();
 
+        /// <summary>
+        /// Checks if user is banned from game
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>True if banned, false if not</returns>
         [OperationContract]
         bool IsUserBanned(int userId);
     }
 
+    /// <summary>
+    /// Handles user connections
+    /// </summary>
     [ServiceContract]
     public interface IUserConnectionHandler
     {
+        /// <summary>
+        /// Notifies user log in to server
+        /// </summary>
+        /// <param name="user"></param>
         [OperationContract]
         void NotifyLogIn(UserDTO user);
 
+        /// <summary>
+        /// Notifies log out to server and closes party resources if on-party
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="isHost"></param>
         [OperationContract]
         void NotifyLogOut(UserDTO user, bool isHost);
 
+        /// <summary>
+        /// Notifies if a guest has left a party or game
+        /// </summary>
+        /// <param name="partyCode"></param>
+        /// <param name="username"></param>
         [OperationContract]
         void NotifyGuestLeft(int partyCode, string username);
 
+        /// <summary>
+        /// Logs out user from online users
+        /// </summary>
+        /// <param name="username"></param>
         [OperationContract]
         void LogOut(string username);
     }
